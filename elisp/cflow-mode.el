3cad0c54c848acbe23713762f4b7c5d4777164d4
Sergey Poznyakoff <gray@gnu.org>
2019-02-08 12:21:55 +0200

    Update copyright statements

--- elisp/cflow-mode.el
+++ elisp/cflow-mode.el
@@ -6,8 +6,7 @@
 ;; $Id$
 
 ;; This file is part of GNU cflow
-;; Copyright (C) 1994-1995, 2005, 2007, 2010, 2014-2017 Sergey
-;; Poznyakoff
+;; Copyright (C) 1994-2019 Sergey Poznyakoff
  
 ;; GNU cflow is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
